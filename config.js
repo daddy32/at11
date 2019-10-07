d22eeb2568acbcf85a9b5e6962241b0a5a865b6e
Kozej Peter <pkozej@skytoll.sk>
2019-10-07 14:04:24 +0200

    TODO list.

--- config.js
+++ config.js
@@ -60,3 +60,35 @@ module.exports = {
         'telka': { name: 'Telka', template: '../views/index5.html' }
     }
 };
+
+/*
+TODO: Nove restauracie vo Westend Plazza:
+    - Quan Ngon - Vietnamese Cuisine
+        - Bez menu online?
+        - https://www.westend.sk/kto-u-nas-sidli/quanngon/
+        - https://menucka.sk/ulica/Quan%20Ngon,%20Lama%C4%8Dsk%C3%A1%20cesta,%20Karlova%20Ves,%20Slovensko
+    - Greek Style
+        - Bez menu online?
+        - https://www.westend.sk/kto-u-nas-sidli/greekstyle/
+    - Thali - 100% Veggie
+        - Bez menu online? Resp maju v pdf formate; spolocne pre vsetky prevadzky?
+        - http://www.thali.sk/najdi-nas/
+    - Bigger - the best snack in the world
+        - http://bigger.sk/lamacska
+        - Menu (na cely tyzden rovnake) tu: http://bigger.sk/lamacska#menu
+            (div.menu-list)
+    - Sushi Time
+        - Nemaju denne menu (vzdy to iste v ponuke)?
+        - https://www.sushitime.sk/sk/menu/
+    - Sajado
+        - Nemaju denne menu (vzdy to iste v ponuke)?
+        - http://www.sajado.sk/-menu-expres-westend
+        - https://restauracie.sme.sk/restauracia/restauracia-sajado_3833-bratislava_2983/denne-menu
+    - fresh garden salads
+        - Nemaju denne menu (vzdy to iste v ponuke)?
+        - http://www.freshgarden.sk/sk/menu/
+    - Anatolia Kebab
+        - Nemaju denne menu (vzdy to iste v ponuke)?
+        - http://www.anatolia.sk/
+TODO: Zvazit pridanie zoznamu liniek na neparsovane restauracie
+*/
