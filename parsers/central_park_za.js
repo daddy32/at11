a99907cde6969ce543c628ed712cd7959eef1de1
daddy32 <pkozej@skytoll.sk>
2020-08-04 15:00:40 +0200

    typo fix

--- parsers/central_park_za.js
+++ parsers/central_park_za.js
@@ -8,24 +8,24 @@ module.exports.parse = function(html, date, callback) {
     var junkPattern = /2 polievky|Dezert|Týždenné menu/;
 
     var denneMenu = parserUtil.findMenuSme($, date);
-    
+
     denneMenu.first().find('.jedlo_polozka').each(function() {
         if ($(this).find('.left>b').length === 0 && !(junkPattern.test($(this).text()))) {
             dayMenu.push(this);
         }
     });
-sdd
+
     //convert to menu item object
     dayMenu = dayMenu.map(function(item) {
         var label = $('.left', item).text();
         var price = $('.right', item).text();
-        return { 
-            isSoup: soupPattern.test(label.trim()), 
-            text: normalize(label), 
-            price: parseFloat(price) 
+        return {
+            isSoup: soupPattern.test(label.trim()),
+            text: normalize(label),
+            price: parseFloat(price)
         };
     });
-    
+
     callback(dayMenu);
 
     function normalize(str) {
