d8fee21b2d88233740c449264e87bb1b9384a827
Kozej Peter <pkozej@skytoll.sk>
2020-09-25 10:43:52 +0200

    Canteen Priatelia cleanup

--- parsers/patronka/priatelia.ts
+++ parsers/patronka/priatelia.ts
@@ -5,26 +5,27 @@ import { Menucka } from "../menucka";
 export class Priatelia extends Menucka implements IParser {
     public parse(html: string, date: Date, doneCallback: (menu: IMenuItem[]) => void): void {
         const menuItems = super.parseBase(html, date);
-        const junkPattern  = /\/\s*A:/
-        const junkPattern2 = /\/\s*A\s*:(\d+\s*,*)+.*/
-        const junkPattern3 = /facebook/i
+        const junkPatternMeal  = /\/\s*A:/
+        const junkPatternSoup = /\/\s*A\s*:(\d+\s*,*)+.*/
+        const junkPattern3 = /facebook|POLIEVKA K/i
 
         if (menuItems.length > 0) {
             menuItems.forEach((item, i) => {
                 //console.log(item.text);
                 if (i==0) {
                     item.isSoup = true;
-                    item.text = item.text.replace(junkPattern2, '');
+                    item.text = item.text.replace(junkPatternSoup, '');
                 }
 
                 if (junkPattern3.test(item.text)) {
                     item.text = '';
+                    item.price = 0;
                 } else {
                     item.text = item.text
                         .removeAlergens()
                         .removeMetrics()
                         .removeItemNumbering()
-                        .replace(junkPattern, '');
+                        .replace(junkPatternMeal, '');
                 }
                 //console.log('   => ', item.text);
             });
