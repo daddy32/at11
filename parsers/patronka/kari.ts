73bd58dbb78a86ad48a000b9c40c83891abc89ee
Kozej Peter <pkozej@skytoll.sk>
2023-08-22 12:03:12 +0200

    Kari - fix

--- parsers/patronka/kari.ts
+++ parsers/patronka/kari.ts
@@ -7,11 +7,15 @@ export class Kari extends Sme implements IParser {
     public parse(html: string, date: Date, doneCallback: (menu: IMenuItem[]) => void): void {
         const menuItemsOrig = super.parseBase(html, date);
         var menuItemsFinal: IMenuItem[] = [];
-        const junkPattern = /(\d+\.*){1,}/;
+        const junkPattern = /(\d+\.*){1,}$/;
         var seenSoup = false;
+        // console.log("Parsing Kari.");
+        // console.log("   date:" + date);
 
         menuItemsOrig.forEach(item=> {
+            // console.log("       text:" + item.text);
             if ((junkPattern.test(item.text))) {
+                // console.log("           Junk");
                 return;
             }
             const result = parsePrice(item.text);
@@ -21,7 +25,7 @@ export class Kari extends Sme implements IParser {
                 seenSoup = true;
             }
 
-            item.text = result.text.replace(/^.*\|\s+/, "").replace(/\(obsahuje:/, "").removeAlergens();
+            item.text = result.text.replace(/^(\d+\.*){1,}/, "").replace(/^.*\|\s+/, "").replace(/\(obsahuje:/, "").removeAlergens();
             menuItemsFinal.push(item)
         });
 
